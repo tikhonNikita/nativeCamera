7491875465b89d00bfa8de83cb3393bcc680e21f
Nikita Tikhonov <jagarnikita@gmail.com>
2023-05-23 19:44:29 +0300

    update request permission view

--- android/app/src/main/java/com/nativecamera/camera/components/Permission.kt
+++ android/app/src/main/java/com/nativecamera/camera/components/Permission.kt
@@ -1,32 +1,52 @@
 package com.nativecamera.camera.components
 
-import android.Manifest
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.*
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-private fun FeatureThatRequiresCameraPermission(
+fun PermissionView(
     cameraPermissionState: PermissionState
 ) {
-    Column {
-        val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
-            // If the user has denied the permission but the rationale can be shown,
-            // then gently explain why the app requires this permission
-            "The camera is important for camera app. Please grant the permission."
-        } else {
-            // If it's the first time the user lands on this feature, or the user
-            // doesn't want to be asked again for this permission, explain that the
-            // permission is required
-            "Camera permission required for this feature to be available. " +
-                    "Please grant the permission"
-        }
-        Text(textToShow)
-        Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
-            Text("We need it to take photo")
+    val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+        stringResource(id = com.nativecamera.R.string.camera_rationale)
+    } else {
+        stringResource(id = com.nativecamera.R.string.camera_rationale_extended)
+    }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(48.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(id = com.nativecamera.R.string.rationale_title),
+            style = MaterialTheme.typography.h4,
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(textToShow, style = MaterialTheme.typography.body2)
+            Spacer(modifier = Modifier.height(48.dp))
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text(text = stringResource(id = com.nativecamera.R.string.request_permission))
+            }
         }
     }
 }
