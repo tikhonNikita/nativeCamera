a0c857a17b5e64c925e4e199e15afafbcf95bdca
Nikita Tikhonov <jagarnikita@gmail.com>
2023-05-27 13:19:38 +0300

    setup basic camera functionality

--- android/app/src/main/java/com/nativecamera/CameraViewManager.kt
+++ android/app/src/main/java/com/nativecamera/CameraViewManager.kt
@@ -6,16 +6,34 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.nativecamera.camera.CameraScreen
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraViewManager: SimpleViewManager<View>() {
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
     override fun getName(): String = "CameraView"
 
     override fun createViewInstance(reactApplicationContext: ThemedReactContext): View {
         return ComposeView(reactApplicationContext).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                CameraScreen()
+                CameraScreen(reactApplicationContext, cameraExecutor, outputDirectory)
             }
+            cameraExecutor = Executors.newSingleThreadExecutor()
+            outputDirectory = getOutputDirectory(reactApplicationContext)
         }
+    }
+
+    private fun getOutputDirectory(reactApplicationContext: ThemedReactContext): File {
+        reactApplicationContext.apply {
+            val mediaDir = externalMediaDirs.firstOrNull()?.let {
+                File(it, "NativeCamera").apply { mkdirs() }
+            }
+            return if (mediaDir != null && mediaDir.exists())
+                mediaDir else reactApplicationContext.filesDir
+        }
+
     }
 }
--- android/app/src/main/java/com/nativecamera/camera/CameraScreen.kt
+++ android/app/src/main/java/com/nativecamera/camera/CameraScreen.kt
@@ -1,24 +1,51 @@
 package com.nativecamera.camera
 
 import android.Manifest
+import android.net.Uri
+import android.util.Log
+import androidx.camera.camera2.internal.annotation.CameraExecutor
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.facebook.react.uimanager.ThemedReactContext
 import com.google.accompanist.permissions.*
 import com.nativecamera.BaseComposeView
 import com.nativecamera.camera.components.Camera
 import com.nativecamera.camera.components.PermissionView
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CameraScreen() {
+fun CameraScreen(reactApplicationContext: ThemedReactContext, cameraExecutor: ExecutorService, outputDirectory:File ) {
+
     val cameraPermissionState = rememberPermissionState(
         Manifest.permission.CAMERA
     )
+
+    DisposableEffect(Unit) {
+        onDispose {
+            cameraExecutor.shutdown()
+        }
+    }
+
     BaseComposeView {
         if (cameraPermissionState.status.isGranted) {
-            Camera()
+            Camera(
+                outputDirectory = outputDirectory,
+                executor = cameraExecutor,
+                onImageCaptured = ::handleImageCapture,
+                onError = {Log.e("Camera", it.toString())}
+            )
         } else {
             PermissionView(cameraPermissionState)
         }
     }
+}
+
+private fun handleImageCapture(uri: Uri): Unit {
+    Log.d("KOSMOSILDLO", uri.toString())
 }
